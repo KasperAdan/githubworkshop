dc400a8e5c0690444d9c49b3bcc54f00234b202f
KasperAdan <57485212+KasperAdan@users.noreply.github.com>
2020-02-12 13:18:29 +0100

    A new method to square 1 variable

--- src/avans/Main.java
+++ src/avans/Main.java
@@ -3,15 +3,21 @@ package avans;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test my methods:");
+        System.out.println("Test my methods:\n");
 
         double a = 33.3;
         double b = 66.6;
         double r = sum(a, b);
+        double s = square(a);
         System.out.println(a + " + " + b + " = " + r);
+        System.out.println(a + " ^2 = " + s);
     }
 
     public static double sum ( double a, double b ) {
         return a + b;
+    }
+    
+    public static double square(double a) {
+        return Math.pow(a, 2);
     }
 }
